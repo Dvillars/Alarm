57f9ee438fdd75745faa390d0be3c1c61d59e293
Derek Villars and Minh Phuong <derek+minh@>
2017-03-14 15:06:48 -0700

    add alert

--- js/time.js
+++ js/time.js
@@ -15,15 +15,14 @@ Alarm.prototype.getCurrentTime = function() {
   });
 }
 
-Alarm.prototype.getUp = function(string)
-{
+Alarm.prototype.getUp = function(string) {
   $.get('http://api.timezonedb.com/v2/get-time-zone?key='+ apiKey + '&format=json&by=zone&zone=America/Los_Angeles').then(function(response) {
     var then = string;
     var rightNow = response.formatted;
     var difference = moment.duration(moment(then).diff(moment(rightNow)));
     console.log(difference._milliseconds);
 
-setTimeOut
+    setTimeout(function() { alert("We are the Campions My Friend, We'll keep on Fighting 'till the end"); }, difference._milliseconds);
   })
   .fail(function(error) {
     $('#error').text(error.responseJSON.message);
